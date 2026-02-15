35b360dbff27025a8841440eb44ad4ba0cfd944e
dlatushkiin-ya <denys.latushkin@yalantis.net>
2026-02-15 19:10:10 +0200

    HUDScene isn't stopped in WinScene

--- js/scenes/WinScene.js
+++ js/scenes/WinScene.js
@@ -85,7 +85,7 @@ PVZ.WinScene = class WinScene extends Phaser.Scene {
             nextBtn.on('pointerdown', () => {
                 PVZ.SoundManager.play('button_click');
                 this.scene.stop('GameScene');
-                this.scene.stop('HUDScene');
+                // this.scene.stop('HUDScene');
                 this.scene.stop();
                 this.scene.start('GameScene', { levelId: nextLevelId });
             });
