cfdb80230eae9fc924bc5ddf7f6b1c301870370a
dlatushkiin-ya <denys.latushkin@yalantis.net>
2026-02-15 16:09:14 +0200

    HUD scene top removed from WinScene

--- js/scenes/WinScene.js
+++ js/scenes/WinScene.js
@@ -103,7 +103,7 @@ PVZ.WinScene = class WinScene extends Phaser.Scene {
         menuBtn.on('pointerdown', () => {
             PVZ.SoundManager.play('button_click');
             this.scene.stop('GameScene');
-            this.scene.stop('HUDScene');
+            // this.scene.stop('HUDScene');
             this.scene.stop();
             this.scene.start('LevelSelectScene');
         });
